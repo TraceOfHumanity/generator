e356a1ea6444d43b5e45c2ff6e786cf12ab1b5f9
Alexandr_Ovchar <azalexxx02@gmail.com>
2023-04-26 18:32:22 +0300

    style checkbox

--- src/components/PasswordGenerator.jsx
+++ src/components/PasswordGenerator.jsx
@@ -49,11 +49,11 @@ function PasswordGenerator() {
   };
 
   return (
-    <div className="flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-2xl bg-black bg-opacity-30 backdrop-blur-[2px] text-white border-[#00eaff] border-b-[1px] border-r-2 p-2 w-80">
+    <div className="flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-2xl bg-black bg-opacity-30 backdrop-blur-[2px] text-white border-[#00eaff] border-b-[1px] border-r-2 p-2 w-80 max-w-[90%] ">
       <h1 className="gradient-text uppercase flex justify-center text-2xl font-semibold	mb-3">
         password generator
       </h1>
-      <label style={{ "--i": "0" }} className="gradient-text font-bold">
+      <label className=" gradient-text font-bold">
         Password Length:
         <span className="ml-2 text-xl ">{passwordLength}</span>
         <input
@@ -64,49 +64,59 @@ function PasswordGenerator() {
           value={passwordLength}
           onChange={(e) => setPasswordLength(e.target.value)}
         />
+        <span className="checkmark"></span>
       </label>
 
-      <label className="gradient-text font-bold">
+      <label className="checkbox gradient-text font-bold">
         Special Characters:
         <input
           type="checkbox"
           checked={includeSpecialChars}
           onChange={(e) => setIncludeSpecialChars(e.target.checked)}
         />
+        <span className="checkmark"></span>
       </label>
-      <label className="gradient-text font-bold">
+      <label className="checkbox gradient-text font-bold">
         Numbers:
         <input
           type="checkbox"
           checked={includeNumbers}
           onChange={(e) => setIncludeNumbers(e.target.checked)}
         />
+        <span className="checkmark"></span>
       </label>
 
-      <label className="gradient-text font-bold">
+      <label className="checkbox gradient-text font-bold">
         Uppercase:
         <input
           type="checkbox"
           checked={includeUpperCase}
           onChange={(e) => setIncludeUpperCase(e.target.checked)}
         />
+        <span className="checkmark"></span>
       </label>
 
-      <label className="gradient-text font-bold">
+      <label className="checkbox gradient-text font-bold">
         Avoid Duplicates:
         <input
           type="checkbox"
           checked={avoidDuplicates}
           onChange={(e) => setAvoidDuplicates(e.target.checked)}
         />
+        <span className="checkmark"></span>
       </label>
+      {/* <label className="checkbox">
+        <input type="checkbox" />
+        <span className="checkmark"></span>
+        Label Text
+      </label> */}
 
-      <button className="btn " onClick={handleGeneratePassword}>
+      <button className="btn gradient-text" onClick={handleGeneratePassword}>
         <p>Generate Password</p>
       </button>
-      <div className="flex ">
-        <p className="gradient-text justify-between w-full">{password}</p>
-        <button onClick={handleCopyPassword}>
+      <div className="flex gradient-text">
+        <p className=" text-sm justify-between w-full">{password}</p>
+        <button className="text-[#00eaff]" onClick={handleCopyPassword}>
           <RxCopy />
         </button>
       </div>
